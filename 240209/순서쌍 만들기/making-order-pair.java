6ef9621e684d6e80292e8be97b7729a73cc96e86
soyun <89512178+jsoyun@users.noreply.github.com>
2024-02-09 22:29:41 +0900

    Title: 순서쌍 만들기, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240209/순서쌍 만들기/making-order-pair.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for(int i =a; i>0; i--){
+            for(int j=a; j>0; j--){
+                System.out.print("("+i+","+j+") ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
